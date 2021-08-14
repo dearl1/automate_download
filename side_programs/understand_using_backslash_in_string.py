4fb1c51c8b95198ddce22d5bcdc3a7698d236649
DEarl <59830842+dearl1@users.noreply.github.com>
2021-08-14 19:36:23 +0100

    Understand how to use escape backslash to have a backslash in a string

---
+++ side_programs/understand_using_backslash_in_string.py
@@ -0,0 +1,17 @@
+
+root_location = "C:\\Users\\danny\\Documents\\1 - Not backed up to external hard drive yet\\automate_download\\"
+##root_location = "C:\\"
+print(root_location)
+
+
+
+
+
+
+
+
+
+
+
+
+
