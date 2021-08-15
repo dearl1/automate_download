7c48f9a1599810cf3160737c44cfbac6c4a9eaf6
DEarl <59830842+dearl1@users.noreply.github.com>
2021-08-15 12:45:12 +0100

    Understand how to filter hrefs according to what they contain

---
+++ side_programs/understand_search_for_substring_in_string.py
@@ -0,0 +1,20 @@
+
+myString = "https://bb.imperial.ac.uk/bbcswebdav/pid-2087956-dt-content-rid-8513478_1/xid-8513478_1"
+myString = "https://www.imperial.ac.uk/about-the-site/use-of-cookies/"
+
+if "xid" in myString:
+    print("Found xid")
+else:
+    print("Did not find xid")
+
+
+
+
+
+
+
+
+
+
+
+
